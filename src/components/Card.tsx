85e8bd4d4be27ace42b2c96f10ac9b6f8fc1028b
Adam Johnson <adaojohnson902@gmail.com>
2024-07-26 21:57:35 -0600

    created initial iteration of card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,36 @@
+﻿type CardProps = {
+  image: string;
+  side?: "left" | "right";
+  content: string;
+};
+
+export default function Card({ image, side = "right", content }: CardProps) {
+  if (side === "left") {
+    return (
+      <div className="card card-side flex flex-row m-8">
+        <div className="card-body w-full">
+          <h2 className="card-title flex justify-center">Vi Heinz</h2>
+          <p className="flex items-center">{content}</p>
+        </div>
+        <figure className="p-8">
+          <img src={image} />
+        </figure>
+      </div>
+    );
+  }
+
+  return (
+    <div className="card card-side flex flex-row m-8">
+      {" "}
+      <figure className="p-8">
+        <img src={image} />
+      </figure>
+      <div className="card-body w-full">
+        <h2 className="card-title flex justify-center">Dr. Gregg Stoner</h2>
+        <div className="mt-4">
+          <p className="my-4">{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
