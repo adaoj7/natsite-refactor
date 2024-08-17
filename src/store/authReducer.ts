14e000431b353c5872e9ac56916bcaa0466fa8a6
Adam Johnson <adaojohnson902@gmail.com>
2024-08-17 16:56:17 -0600

    initial setup of redux store

---
+++ src/store/authReducer.ts
@@ -0,0 +1,46 @@
+﻿import { UnknownAction } from "@reduxjs/toolkit";
+
+export interface LoginAction {
+  type: "LOGIN";
+  payload: {
+    user: string;
+    name: string;
+    email: string;
+    isAdmin: boolean;
+  };
+}
+
+export interface LogoutAction {
+  type: "LOGOUT";
+}
+
+export type ActionType = LoginAction | LogoutAction | UnknownAction;
+
+const initialState = {
+  user: "",
+  name: "",
+  email: "",
+  isAdmin: false,
+};
+
+const authReducer = (state = initialState, action: ActionType) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        // @ts-expect-error - action is of type ActionLogin
+        user: action.payload.user,
+        // @ts-expect-error - action is of type ActionLogin
+        name: action.payload.name,
+        // @ts-expect-error - action is of type ActionLogin
+        email: action.payload.email,
+        // @ts-expect-error - action is of type ActionLogin
+        isAdmin: action.payload.isAdmin,
+      };
+    case "LOGOUT":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+﻿import authReducer from "./authReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { UnknownAction } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export default store;
