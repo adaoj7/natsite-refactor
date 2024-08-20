65672a1449cc7efc32c8b873de16df4fd1077cfe
Adam Johnson <adaojohnson902@gmail.com>
2024-08-19 22:24:10 -0600

    created app component and new router from elements to be able to handle authentication for routes

---
+++ src/app.tsx
@@ -0,0 +1,95 @@
+﻿import React from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Root from "./root";
+import Home from "./routes/home";
+import About from "./routes/about";
+import Setup from "./routes/get-involved-routes/setup";
+import Host from "./routes/get-involved-routes/host";
+import MyShifts from "./routes/get-involved-routes/my-shifts";
+import Donate from "./routes/get-involved-routes/donate";
+import ThisYear from "./routes/this-year";
+import Gallery from "./routes/gallery";
+import LightTheWorld from "./routes/light-the-world";
+import Contact from "./routes/contact";
+import ErrorPage from "./error-page";
+import GetInvolved from "./routes/get-involved";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { isAuthenticated } = useAuth0();
+
+  const router2 = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/getInvolved" element={<GetInvolved />}>
+          <Route path="/getInvolved/setup" element={<Setup />} />
+          <Route path="/getInvolved/host" element={<Host />} />
+          <Route path="/getInvolved/myShifts" element={<MyShifts />} />
+          <Route path="/getInvolved/donate" element={<Donate />} />
+        </Route>
+        <Route path="/thisYear" element={<ThisYear />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/lightTheWorld" element={<LightTheWorld />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    )
+  );
+
+  const router = createBrowserRouter([
+    {
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/getInvolved",
+          element: <GetInvolved />,
+          children: [
+            {
+              path: "/getInvolved/setup",
+              element: <Setup />,
+            },
+            {
+              path: "/getInvolved/host",
+              element: <Host />,
+            },
+            {
+              path: "/getInvolved/myShifts",
+              element: <MyShifts />,
+            },
+            {
+              path: "/getInvolved/donate",
+              element: <Donate />,
+            },
+          ],
+        },
+        { path: "/thisYear", element: <ThisYear /> },
+        { path: "/gallery", element: <Gallery /> },
+        { path: "/lightTheWorld", element: <LightTheWorld /> },
+        { path: "/contact", element: <Contact /> },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router2} />
+    </>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,68 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import App from "./App.tsx";
-import Root from "./root.tsx";
-import Home from "./routes/home.tsx";
-import About from "./routes/about.tsx";
-import Setup from "./routes/get-involved-routes/setup.tsx";
-import Host from "./routes/get-involved-routes/host.tsx";
-import MyShifts from "./routes/get-involved-routes/my-shifts.tsx";
-import Donate from "./routes/get-involved-routes/donate.tsx";
-import ThisYear from "./routes/this-year.tsx";
-import Gallery from "./routes/gallery.tsx";
-import LightTheWorld from "./routes/light-the-world.tsx";
-import Contact from "./routes/contact.tsx";
-import ErrorPage from "./error-page.tsx";
-import "./index.css";
-import GetInvolved from "./routes/get-involved.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./app.tsx";
 
 const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/getInvolved",
-        element: <GetInvolved />,
-        children: [
-          {
-            path: "/getInvolved/setup",
-            element: <Setup />,
-          },
-          {
-            path: "/getInvolved/host",
-            element: <Host />,
-          },
-          {
-            path: "/getInvolved/myShifts",
-            element: <MyShifts />,
-          },
-          {
-            path: "/getInvolved/donate",
-            element: <Donate />,
-          },
-        ],
-      },
-      { path: "/thisYear", element: <ThisYear /> },
-      { path: "/gallery", element: <Gallery /> },
-      { path: "/lightTheWorld", element: <LightTheWorld /> },
-      { path: "/contact", element: <Contact /> },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -72,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       authorizationParams={{ redirect_uri: window.location.origin }}
     >
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <App />
       </QueryClientProvider>
     </Auth0Provider>
   </React.StrictMode>
