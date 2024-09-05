58165d5c9d896583815f2908cd0d9426ebdc62ad
Adam Johnson <adaojohnson902@gmail.com>
2024-09-05 15:12:18 -0600

    fix last few ts errors

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,4 @@
 ﻿import HeroImage from "../components/Hero";
-import DesktopBackground from "../assets/backgrounds/Background-Desktop-Edited.jpg";
-import Logo from "../assets/logos/CFN White-01.png";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 
