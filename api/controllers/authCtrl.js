63d912035ce4dcf6ae6c7a5598ee92bbdf8af64a
Adam Johnson <adaojohnson902@gmail.com>
2024-09-27 21:25:24 -0600

    comments for auth

--- api/controllers/authCtrl.js
+++ api/controllers/authCtrl.js
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 export default {
   // Do I want to use Auth0 so that users can reset passwords if needed?
   // I need a way for users to reset their passwords if they forget them and also authentication is handled really well by Auth0 already so I think that it would be a good idea to use it.
+  // I think I need to add admin linking to the db from auth0
+  // I need to see if there is a less round about way of doing what I am doing for auth right now
   login: async (req, res) => {
     try {
       const { name, email } = req.body;
