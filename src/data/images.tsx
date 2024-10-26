ae4074706d6e86e93189bc01bb4d29a35c5b3e09
Adam Johnson <adaojohnson902@gmail.com>
2024-10-26 03:40:36 -0600

    changed images in gallery to nativities

--- src/data/images.tsx
+++ src/data/images.tsx
@@ -1,33 +1,75 @@
-﻿import image1 from "../assets/site-images/light-the-world/LTW2016.jpg";
-import image2 from "../assets/site-images/light-the-world/LTW2017.jpg";
-import image3 from "../assets/site-images/light-the-world/LTW2018.jpg";
-import image7 from "../assets/site-images/light-the-world/LTW2022.jpg";
-import image8 from "../assets/site-images/light-the-world/LTW2023.jpg";
+﻿import BelizeNativity from "../assets/site-images/gallery-photos/BelizeNativity.jpg";
+import BigNativity from "../assets/site-images/gallery-photos/BigNativity.jpg";
+import ChinaNativity from "../assets/site-images/gallery-photos/ChinaNativity.jpg";
+import ChinaNativity2 from "../assets/site-images/gallery-photos/ChinaNativity2.jpg";
+import ColombiaNativity from "../assets/site-images/gallery-photos/ColombiaNativity.jpg";
+import GuatemalaNativity from "../assets/site-images/gallery-photos/GuatemalaNativity.jpg";
+import EcuadorNativity from "../assets/site-images/gallery-photos/EcuadorNativity.jpg";
+import GermanyEstoniaNativities from "../assets/site-images/gallery-photos/GermanyEstoniaNativities.jpg";
+import IsraelNativity from "../assets/site-images/gallery-photos/IsraelNativity.jpg";
+import PolandNativity from "../assets/site-images/gallery-photos/PolandNativity.jpg";
+import SwedenNativity from "../assets/site-images/gallery-photos/SwedenNativity.jpg";
+import RandomNativity from "../assets/site-images/gallery-photos/71 ,2018 Festival of Nativities.jpg";
 
 export const images = [
   {
     id: 1,
-    src: image2,
-    alt: "Image 1",
+    src: BelizeNativity,
+    alt: "Belize Nativity",
   },
   {
     id: 2,
-    src: image1,
-    alt: "Image 2",
+    src: ColombiaNativity,
+    alt: "Colombia Nativity",
   },
   {
     id: 3,
-    src: image7,
-    alt: "Image 3",
+    src: IsraelNativity,
+    alt: "Israel Nativity",
+  },
+  {
+    id: 4,
+    src: RandomNativity,
+    alt: "Random Nativity",
+  },
+  {
+    id: 5,
+    src: ChinaNativity2,
+    alt: "China Nativity 2",
+  },
+  {
+    id: 6,
+    src: BigNativity,
+    alt: "Big Nativity",
   },
   {
     id: 7,
-    src: image8,
-    alt: "Image 7",
+    src: GuatemalaNativity,
+    alt: "Guatemala Nativity",
   },
   {
     id: 8,
-    src: image3,
-    alt: "Image 8",
+    src: EcuadorNativity,
+    alt: "Ecuador Nativity",
+  },
+  {
+    id: 9,
+    src: GermanyEstoniaNativities,
+    alt: "Germany and Estonia Nativities",
+  },
+  {
+    id: 10,
+    src: ChinaNativity,
+    alt: "China Nativity",
+  },
+  {
+    id: 11,
+    src: PolandNativity,
+    alt: "Poland Nativity",
+  },
+  {
+    id: 12,
+    src: SwedenNativity,
+    alt: "Sweden Nativity",
   },
 ];
